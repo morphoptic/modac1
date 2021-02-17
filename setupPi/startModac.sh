89ac3c2447b949d43c2c0194bcd051433fd8f641
morphoptic <isdale@gmail.com>
2021-02-16 17:25:14 -1000

    piwatcher executable, mods to setupPi

---
+++ setupPi/startModac.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -v
+piwatcher reset
+piwatcher wake 20
+piwatcher watch 60
+cd /home/pi/MODAC_Dev/
+# need this to make pynng work
+PYTHONUNBUFFERED=1;LD_PRELOAD=/usr/lib/arm-linux-gnueabihf/libatomic.so.1
+sleep 3
+LD_PRELOAD=/usr/lib/arm-linux-gnueabihf/libatomic.so.1 python3 /home/pi/MODAC_Dev/modacServer.py 
+python3 relayModuleOff.py
+sleep 5
+
+
