c26cd088db93fe294caaaa036cb51cf241a9572a
morphoptic <isdale@gmail.com>
2021-02-25 15:35:29 -1000

    mod to startModac.sh

--- startScripts/startModac.sh
+++ startScripts/startModac.sh
@@ -1,4 +1,7 @@
 #!/bin/sh -v
+piwatcher reset
+piwatcher wake 30
+piwatcher watch 120
 cd /home/pi/MODAC_Dev/
 # need this to make pynng work
 PYTHONUNBUFFERED=1;LD_PRELOAD=/usr/lib/arm-linux-gnueabihf/libatomic.so.1
@@ -6,4 +9,5 @@ sleep 3
 LD_PRELOAD=/usr/lib/arm-linux-gnueabihf/libatomic.so.1 python3 /home/pi/MODAC_Dev/modacServer.py 
 python3 relayModuleOff.py
 sleep 5
+
 
